302e6d22150cb1e2381c8b557ae42fa519801436
Stefanyds85 <beautyxyzu4@aol.com>
2017-12-03 16:14:31 -0500

    basic start

---
+++ assets/Javascript/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+// var api = RAC4lCleXNU7kZvt4CYXUAEdlO9myO62;  
+
+var celebrityName = ["Kavin Hart","Jay Z", "Michel Obama", "Denzel Washington","Oprah Winfrey"];
+
+
+
+
+
+});
