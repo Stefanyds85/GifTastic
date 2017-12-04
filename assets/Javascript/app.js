f938187a908c7c482cd342e67499f761dd7e1936
Stefanyds85 <beautyxyzu4@aol.com>
2017-12-03 20:20:17 -0500

    non working buttons

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -2,7 +2,38 @@ $(document).ready(function () {
 
 // var api = RAC4lCleXNU7kZvt4CYXUAEdlO9myO62;  
 
-var celebrityName = ["Kavin Hart","Jay Z", "Michel Obama", "Denzel Washington","Oprah Winfrey"];
+
+var topics = ["Kavin Hart","Jay Z", "Michel Obama", "Denzel Washington","Oprah Winfrey"];
+
+
+function displayButtons() {
+
+  for (var i = 0; i < topics.length; i++) {
+
+    var x = $("<button>");
+
+    x.addClass("celebbrity");
+    x.attr("data-name", topics[i]);
+    x.text(topics[i]);
+    $("#giphyBtns").append(x);
+  }
+}
+
+$("#searchGiphy").on("click", function(event) {
+  event.preventDefault();
+
+  var celebrity = $("#celebInput").val().trim();
+  
+  topics.push(celebrity);
+  console.log(topics)
+
+  
+  displayButtons();
+});
+
+
+
+
 
 
 
