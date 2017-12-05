c714769c1818b70579c109495e98dae236973c8c
Stefanyds85 <beautyxyzu4@aol.com>
2017-12-05 18:25:16 -0500

    working pictures

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,79 +1,94 @@
 $(document).ready(function () {
 
-// var api = RAC4lCleXNU7kZvt4CYXUAEdlO9myO62;  
 
 
-var topics = ["Kevin Hart","Jay Z", "Michel Obama", "Denzel Washington","Oprah Winfrey"];
+
+  var topics = ["Kevin Hart", "Jay Z", "Michel Obama", "Denzel Washington", "Oprah Winfrey"];
 
 
-function displayButtons() {
-  
-  $("#giphyBtns").empty();
-  
-  for (var i = 0; i < topics.length; i++) {
-  
-    var gifButton = $("<button>");
+  function displayButtons() {
+    console.log("displayButton");
 
-    gifButton.addClass("celebrity");
-    gifButton.attr("data-name", topics[i]);
-    gifButton.text(topics[i]);
-    $("#giphyBtns").append(gifButton);
+    $("#giphyBtns").empty();
 
+    for (var i = 0; i < topics.length; i++) {
+
+      var gifButton = $("<button>");
+
+      gifButton.addClass("celebrity");
+      gifButton.attr("data-name", topics[i]);
+      gifButton.text(topics[i]);
+      $("#giphyBtns").append(gifButton);
+
+    };
   };
-};
 
-$(".submitBtn").on("click", function(event) {
-  var topicBtn = $(".submitBtn").attr("data-name");
-  event.preventDefault();
-  
-  var celebrity = $(".celebInput").val().trim();
-  
-  topics.push(celebrity);
-  console.log(topics);
+  $(".submitBtn").on("click", function (event) {
+    console.log("submit click");
+
+    var topicBtn = $(".submitBtn").attr("data-name");
+    event.preventDefault();
+
+    var celebrity = $(".celebInput").val().trim();
+
+    if (celebrity) {
+      topics.push(celebrity);
+      console.log(topics);
+
+      displayButtons();
+    }
+
+  });
+
+
+  // $("#showPic").on("click",sutmitBtn, funtion(event){
+
+  function displayCelebrityInfo() {
+
+    console.log("displayCelebrityInfo");
+
+    var apiKey = "RAC4lCleXNU7kZvt4CYXUAEdlO9myO62";
+    console.log($(this));
+
+    var celebrityName = $(this).attr("data-name");
+    console.log("celbrityName:", celebrityName);
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + celebrityName + "&api_key=" + apiKey + "&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function (response) {
+
+      var results = response.data
+
+      // console.log("resutls:", results);
+
+      // results.forEach(function (item, index) {
+
+      $("#celebGiphy").empty();
+
+      for (var i = 0; i < results.length; i++) {
+        console.log("i:", i);
+        var image = $("<img>").attr("src", results[i].images.fixed_height_still.url);
+        // var celebPic = response.images;
+
+        var rated = $("<p>").text("Rated:" + results[i].rating);
+        // var rating = response.rating;
+
+        $("#celebGiphy").append(image);
+      }
+
+      // displayButtons();
+    });
+    // });
+  };
+
+  $("#giphyBtns").on("click", ".celebrity", displayCelebrityInfo);
 
   displayButtons();
-  
+  // displayCelebrityInfo()
+
 });
 
-
-
-function displayCelebrityInfo() {
-  v
-  var celebrityName = $("#celebGiphy").attr("data-name");
-          var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=RAC4lCleXNU7kZvt4CYXUAEdlO9myO62&q=celebrity+name&limit=10";
-  
-          $.ajax({
-            url: queryURL,
-            method: "GET"
-          }).done(function(response) {
-           
-            var results = response.data
-            
-            results.forEach (function (item, index) {
-
-            var image = $("<img>").attr("src", results[index].images.fixed_height_still.url);
-            // var celebPic = response.images;
-
-            var rated = $("<p>").text("Rated:" + results[index].rating);
-            // var rating = response.rating;
-
-            $("#celebGiphy").empty();
-            $("#celebGiphy").append(image, rated);
-            
-            
-            displayButtons();
-            });
-          });
-        };
-
-        $("#giphyBtns").on("click", displayCelebrityInfo);
-        
-         displayButtons();
-         displayCelebrityInfo()
-});
-
-
-
-
-
-
+// });
