a85e727aa8c666f5e6650361210aecd9fabd1231
Stefanyds85 <beautyxyzu4@aol.com>
2017-12-05 22:56:05 -0500

    fully working Giftasticgit add -A

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
       url: queryURL,
       method: "GET"
     }).done(function (response) {
-
+     
       var results = response.data
 
       // console.log("resutls:", results);
@@ -70,8 +70,12 @@ $(document).ready(function () {
 
       for (var i = 0; i < results.length; i++) {
         console.log("i:", i);
-        var image = $("<img>").attr("src", results[i].images.fixed_height_still.url);
-        // var celebPic = response.images;
+        var image = $("<img>")
+                      .attr("src", results[i].images.fixed_height_still.url)
+                      .attr("data-still", results[i].images.fixed_height_still.url)
+                      .attr("data-animate", results[i].images.fixed_height.url)
+                      .attr("data-state","still")
+                      .addClass("gifImg");
 
         var rated = $("<p>").text("Rated:" + results[i].rating);
         // var rating = response.rating;
@@ -83,6 +87,24 @@ $(document).ready(function () {
     });
     // });
   };
+
+  $("#celebGiphy").on("click", ".gifImg", function(){
+    
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what 
+    // its data-animate value is.
+    // and set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+
+  });
+
 
   $("#giphyBtns").on("click", ".celebrity", displayCelebrityInfo);
 
